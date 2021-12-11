28a0cf9c5fa397782571f767be20aa823af7c44a
Walker_Jermaine@cat.com <Walker_Jermaine@cat.com>
2021-12-11 01:22:16 -0500

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -20,7 +20,7 @@ public class App
         int sum = 0;
 
         for (int i= 0; i < numbers.length; i++) {
-            sum += numbers[0];
+            sum += numbers[i];
         }
 
         return sum;
